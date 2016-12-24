29c684e7d66e6cc9c76b09ead9fbc87ac09b0974
MadMed677 <madmed677@gmail.com>
2016-12-24 04:00:44 +0300

    Add task 5.1

---
+++ 5_1/src/Main.scala
@@ -0,0 +1,46 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import SparkContext._
+
+object Main {
+
+	def main(args: Array[String]) {
+		val appName = "SparkWordCount"
+		val jars = List(SparkContext.jarOfObject(this).get)
+		println(jars)
+		val conf = new SparkConf().setAppName(appName).setJars(jars)
+		val sc = new SparkContext(conf)
+		if (args.length < 2) {
+			println(args.mkString(","))
+			println("ERROR. Please, specify input and output directories.")
+		} else {
+			val inputDir = args(0)
+			val outputDir = args(1)
+			println("Input directory: " + inputDir)
+			println("Output directory: " + outputDir)
+			run(sc, inputDir, outputDir)
+		}
+	}
+
+	def run(sc: SparkContext, inputDir: String, outputDir: String) {
+		val maxEl = sc
+		.textFile(inputDir + "/*")
+		.flatMap(line => line.split("\\W+"))
+		.filter(word => !word.isEmpty())
+		.zipWithIndex()
+		.map(_.swap)
+		.flatMap(pair => Array(pair, (pair._1 + 1, pair._2)))
+		.groupByKey()
+		.filter(it => (it._2.size == 2))
+		.map(_.swap)
+		.map(pair => (pair._1, 1))
+		.reduceByKey((a,b) => a+b)
+		.sortBy(_._2, false)
+		.first()
+
+		val rdd = sc
+		.parallelize(Array(maxEl._1))
+		.map(it => it mkString " ")
+		.saveAsTextFile(outputDir)
+	}
+}
